468c22c5702e44d1db004a72fbfe02debe6110f7
MateusAugustoo <mateusaugusto2017pp@gmail.com>
2024-12-21 02:52:49 -0300

    add username field to user creation route and implement user retrieval by uid ...in process

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,16 +1,53 @@
 import z from "zod";
 import { TFastifyInstance } from "../types/TFastifyInstance";
+import { TUser } from "../types/TUser";
+import { createUser } from "../service/user";
 
 export async function userRouters(app: TFastifyInstance) {
-    app.post("/user", {
-    schema: {
-      body: z.object({
-        uid: z.string(),
-        photo: z.string().optional(),
-        email: z.string().email(),
-        name: z.string().optional(),
-      }),
-      description: "This route creates a new user with the provided uid, email, and optional photo and name fields.",
+  app.post(
+    "/user",
+    {
+      schema: {
+        body: z.object({
+          uid: z.string(),
+          username: z.string(),
+          photo: z.string().optional(),
+          email: z.string().email(),
+          name: z.string().optional(),
+        }),
+        description:
+          "This route creates a new user with the provided uid, email, and optional photo and name fields.",
+        tags: ["User"],
+      },
+    },
+    async (request, reply) => {
+      try {
+        const { uid, username, photo, email, name } = request.body as TUser;
+        const user = await createUser({ uid, username, photo, email, name });
+
+        return reply.code(201).send(user);
+      } catch (error: Error | unknown) {
+        if (error instanceof Error) {
+          reply.code(400).send({ message: error.message });
+        }
+        reply.code(500).send({ message: "Internal server error" });
+      }
     }
-  }, async () => {});
+  );
+
+  app.get(
+    "/user/:uid",
+    {
+      schema: {
+        params: z.object({
+          uid: z.string(),
+        }),
+        description: "This route returns a user by their uid.",
+        tags: ["User"],
+      },
+    },
+    async (request, reply) => {
+      const { uid } = request.params as { uid: string };
+    }
+  );
 }
